1536ecdeff27369a8af9f72a0954be43d7897296
Antonio Trindade <atrindade@umov.me>
2022-12-02 09:21:07 -0300

    Iniciado projeto

---
+++ src/test/java/com/targettrust/test/WebElementsTest.java
@@ -0,0 +1,100 @@
+package com.targettrust.test;
+
+import static org.junit.Assert.*;
+
+import java.util.List;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+public class WebElementsTest {
+	
+	private WebDriver driver;	
+
+	@Before
+	public void setUp() throws Exception {
+		System.setProperty("webdriver.chrome.driver", "/home/antonio/Dev/drivers/chromedriver"); 		
+		driver = new ChromeDriver();
+		driver.get("http://antoniotrindade.com.br/treinoautomacao/elementsweb.html");
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		driver.quit();
+	}
+
+	@Test
+	public void testValidateTitle() {
+		// Valida o título da página
+		
+		String title = driver.getTitle();	
+		assertEquals("WebElements Test Page Lab", title);
+	}
+	
+	@Test
+	public void testWriteTextFieldName() throws InterruptedException {
+		//Localiza elemento
+		WebElement tfName = driver.findElement(By.name("txtbox1"));
+	
+		//Interage com elemento
+		tfName.sendKeys("Antônio");	
+		
+		//opcional sleep
+		Thread.sleep(3000);		
+		
+		String valueExpected = tfName.getAttribute("value"); 
+		
+		//Valida o resultado		
+		assertEquals("O nome deveria ser igual", "Antônio", valueExpected);		
+	}
+	
+	@Test
+	public void testValidateTextFieldEnable() throws InterruptedException {
+		//Localizar os elementos
+		WebElement tfEnable = driver.findElement(By.name("txtbox1"));
+		WebElement tfDisable = driver.findElement(By.name("txtbox2"));
+		
+		//Interaçao com os elementos 
+		//***NÃO PRECISA, SO A NÍVEL DE CURIOSIDADE
+		if (tfEnable.isEnabled()) {
+			tfEnable.sendKeys("habilitado");
+		}
+		
+		if (tfDisable.isEnabled()) {
+			tfDisable.sendKeys("Outro texto qualquer");
+		}	
+		
+		Thread.sleep(3000);
+		
+		//Valida o resultado
+		assertTrue(tfEnable.isEnabled());		
+		assertFalse(tfDisable.isEnabled());					
+	}
+	
+	@Test
+	public void testRadioButton() throws InterruptedException {
+		List<WebElement> listRadio = driver.findElements(By.name("radioGroup1"));
+		
+		assertEquals(4, listRadio.size());
+		
+		//listRadio.get(2).click();
+		
+		for (WebElement element : listRadio) {					
+			if ("Radio 3".equals(element.getAttribute("value"))) {
+				element.click();
+			}
+		}		
+				
+		assertTrue(listRadio.get(2).isSelected());		
+		assertFalse(listRadio.get(0).isSelected());
+		assertFalse(listRadio.get(1).isSelected());
+		assertFalse(listRadio.get(3).isSelected());		
+	}
+	
+}
